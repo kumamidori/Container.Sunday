c71dce8d7a64a749225a35c9a98d9dfb7861cccb
YAMANE Nana <shigematsu.nana@gmail.com>
2020-12-23 01:47:36 +0900

    configure default context hal-app

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use Kumamidori\ExampleContainer\Bootstrap;
 
 require dirname(__DIR__) . '/autoload.php';
-exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER));
+exit((new Bootstrap())('hal-app', $GLOBALS, $_SERVER));
